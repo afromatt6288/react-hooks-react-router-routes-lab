821b72c2375c474f548bd7effe8ab40c2a171565
Matthew Clark <afromatt6288@hotmail.com>
2023-03-02 15:55:43 -0700

    completed lab

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,22 @@
 import React from "react";
 import { movies } from "../data";
 
+
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Movies Page</h1>
+    {movies.map((movie) => (
+    <div key={movie.title}>
+      <h2>{movie.title}</h2>
+      <small>Runtime: {movie.time} min</small>
+      <ul>
+      {movie.genres.map((genre) => (
+        <li key={genre}>{genre}</li>
+      ))}
+      </ul>
+    </div>
+  ))}
+  </div>;
 }
 
 export default Movies;
